4b0363a54ee7053eb355aa1e956a4bca6620a211
root <jjakucionis@icloud.com>
2020-06-18 07:11:15 +0000

    refactor

--- src/FavMoviesList.js
+++ src/FavMoviesList.js
@@ -5,22 +5,18 @@ class FavMoviesList extends React.Component {
     const profiles = this.props.profiles;
     const users = this.props.users;
     const movies = this.props.movies;
-    const favArray = [];
-    
-    profiles.forEach(profile => {
-    	favArray.push([
-          users[profile.userID].name,
-          movies[profile.favoriteMovieID].name
-        ])
-    })
     
   	return (
       <ol>
-      	{favArray.map(item => (
-         	<li key={item[0]}>
-      			{`${item[0]}'s favorite movie is ${item[1]}.`}
+      	{profiles.map(profile => {
+         	const user = users[profile.userID].name;
+    		const movie = movies[profile.favoriteMovieID].name;
+    		return (
+         	<li key={profile.id}>
+              {`${user}'s favorite movie is ${movie}.`}
          	</li>
-         ))}
+			)
+  		})}
       </ol>
     )
   }
